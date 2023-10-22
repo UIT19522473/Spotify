92702809ec25141fee058ede2258cebec942d728
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2023-10-22 22:39:28 +0700

    commit

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "5c4f06cae62746d0bcf6f2cb0d157585";
-const redirecURL = "http://localhost:3000";
+const redirecURL = process.env.REACT_APP_URL_CLIENT;
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirecURL}&scope=${scopes.join(
